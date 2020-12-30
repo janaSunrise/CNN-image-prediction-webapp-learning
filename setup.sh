2129c86c46958757907b5ce15f605a9dec51c658
janaSunrise <warriordefenderz@gmail.com>
2020-12-30 10:48:05 +0530

    Try fixing the runtime.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"warriordefenderz@gmail.com\"\n\
+email = \"\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
